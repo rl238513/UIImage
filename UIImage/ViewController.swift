387ad400f862f927be4be4b385ba6bdd3c66520c
EDUARDO MENDOZA <rl238513@rlas-macair-36.local>
2018-12-11 11:02:43 -0600

    Initial Commit

---
+++ UIImage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UIImage
+//
+//  Created by EDUARDO MENDOZA on 9/6/18.
+//  Copyright © 2018 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Photo: UIImageView!
+   
+    @IBAction func photoSwitch(_ sender: UISwitch) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+   
+    }
+
